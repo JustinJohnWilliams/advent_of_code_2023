b99bfcd4e24b0b6921622b11578bfc2455d1e101
Justin Williams <justinjohnwilliams@gmail.com>
2023-12-05 08:04:55 -0600

    day 4: part 2 (wip)

--- 04/Program.cs
+++ 04/Program.cs
@@ -4,7 +4,7 @@ sw.Start();
 Console.WriteLine($"*************Day 4 START*************");
 
 var p1 = part_one("input.txt");
-var p2 = part_two("input.txt");
+var p2 = part_two("example.txt");
 
 sw.Stop();
 
@@ -35,11 +35,6 @@ Console.WriteLine($"*************Day 4 START*************");
 
     var winning_hands = find_winners_in_games(winning_numbers, chosen_numbers);
 
-    foreach(var winner in winning_hands)
-    {
-        Console.WriteLine($"Game {winner.Key} has winners: {string.Join(",", winner.Value)}");
-    }
-
     var total = 0;
 
     foreach(var winner in winning_hands.Where(c => c.Value.Any()))
@@ -52,30 +47,72 @@ Console.WriteLine($"*************Day 4 START*************");
     return (total, sw.Elapsed.TotalMilliseconds);
 }
 
-Dictionary<int, List<int>> find_winners_in_games(Dictionary<int, List<int>> winning_numbers, Dictionary<int, List<int>> chosen_numbers)
-{
-    var results = new Dictionary<int, List<int>>();
-
-    foreach(var key in winning_numbers.Keys)
-    {
-        if(chosen_numbers.ContainsKey(key))
-        {
-            results.Add(key, winning_numbers[key].Intersect(chosen_numbers[key]).ToList());
-        }
-    }
-
-    return results;
-}
-
 (int result, double ms) part_two(string file)
 {
     var sw = new System.Diagnostics.Stopwatch();
     sw.Start();
 
     var lines = File.ReadAllLines(file);
+    var winning_numbers = new Dictionary<int, List<int>>();
+    var chosen_numbers = new Dictionary<int, List<int>>();
+
+    var game_count = lines.Length;
+
+    foreach(var line in lines)
+    {
+        var id_and_number_parts = line.Split(":", StringSplitOptions.RemoveEmptyEntries);
+        var id = Convert.ToInt32(id_and_number_parts[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+        var winners = id_and_number_parts[1].Split("|")[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        winning_numbers.Add(id, winners.Select(c => Convert.ToInt32(c)).ToList());
+
+        var picks = id_and_number_parts[1].Split("|")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        chosen_numbers.Add(id, picks.Select(c => Convert.ToInt32(c)).ToList());
+    }
+
+    var winning_hands = find_winners_in_games(winning_numbers, chosen_numbers);
+    var prize_cards = new List<KeyValuePair<int, List<int>>>();
+
     var total = 0;
+
+    foreach(var winner in winning_hands)
+    {
+        // Add original card
+        prize_cards.Add(new KeyValuePair<int, List<int>>(winner.Key, winner.Value));
+
+        // get the new game cards
+        var new_card_count = winner.Key + winner.Value.Count > game_count ? (game_count - winner.Key) : winner.Value.Count;
+        //Console.WriteLine($"Card {winner.Key} has {new_card_count} matching numbers");
+
+        for(int i = 1; i <= new_card_count; i++)
+        {
+            //Console.WriteLine($"Adding {winner.Key + i} to {winner.Key}");
+            prize_cards.Add(new KeyValuePair<int, List<int>>(winner.Key + i, winning_numbers[winner.Key + i]));
+        }
+    }
+
+    //foreach(var pc in prize_cards.OrderBy(c => c.Key))
+    //{
+    //   Console.WriteLine($"{pc.Key}: {string.Join(",", pc.Value)}");
+    //}
+
+    //Console.WriteLine($"Total is: {prize_cards.Count}");
 
     sw.Stop();
 
     return (total, sw.Elapsed.TotalMilliseconds);
+}
+
+List<KeyValuePair<int, List<int>>> find_winners_in_games(Dictionary<int, List<int>> winning_numbers, Dictionary<int, List<int>> chosen_numbers)
+{
+    var results = new List<KeyValuePair<int, List<int>>>();
+
+    foreach(var key in winning_numbers.Keys)
+    {
+        if(chosen_numbers.ContainsKey(key))
+        {
+            results.Add(new KeyValuePair<int, List<int>>(key, winning_numbers[key].Intersect(chosen_numbers[key]).ToList()));
+        }
+    }
+
+    return results;
 }

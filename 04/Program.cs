c078a6988963462a2b5a237ed00cd4a38d67694c
Justin Williams <justinjohnwilliams@gmail.com>
2023-12-06 11:22:51 -0600

    clean up clean up

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,19 +1,13 @@
-﻿var sw = new System.Diagnostics.Stopwatch();
-sw.Start();
-
-Console.WriteLine($"*************Day 4 START*************");
+﻿Console.WriteLine($"*************Day 4 START*************");
 
 var p1 = part_one("input.txt");
 var p2 = part_two("example.txt");
 
-sw.Stop();
-
 Console.WriteLine($"Part 1 Result: {p1.result} \t: {p1.ms}ms");
 Console.WriteLine($"Part 2 Result: {p2.result} \t: {p2.ms}ms");
-Console.WriteLine($"Time (total)\t\t: {sw.Elapsed.TotalMilliseconds}ms");
-Console.WriteLine($"*************Day 4 START*************");
+Console.WriteLine($"*************Day 4  DONE*************");
 
-(int result, double ms) part_one(string file)
+(long result, double ms) part_one(string file)
 {
     var sw = new System.Diagnostics.Stopwatch();
     sw.Start();
@@ -47,7 +41,7 @@ Console.WriteLine($"*************Day 4 START*************");
     return (total, sw.Elapsed.TotalMilliseconds);
 }
 
-(int result, double ms) part_two(string file)
+(long result, double ms) part_two(string file)
 {
     var sw = new System.Diagnostics.Stopwatch();
     sw.Start();

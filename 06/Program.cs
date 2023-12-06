db160bc3f8c44fb5d9d3bdd62d37846abcbbccc9
Justin Williams <justinjohnwilliams@gmail.com>
2023-12-06 09:44:02 -0600

    day 6: part 2

--- 06/Program.cs
+++ 06/Program.cs
@@ -1,17 +1,17 @@
 ﻿var sw = new System.Diagnostics.Stopwatch();
 sw.Start();
 
-Console.WriteLine($"*************Day 4 START*************");
+Console.WriteLine($"*************Day 6 START*************");
 
 var p1 = part_one("input.txt");
-var p2 = part_two("example.txt");
+var p2 = part_two("input.txt");
 
 sw.Stop();
 
-Console.WriteLine($"Part 1 Result: {p1.result} \t: {p1.ms}ms");
+Console.WriteLine($"Part 1 Result: {p1.result} \t\t: {p1.ms}ms");
 Console.WriteLine($"Part 2 Result: {p2.result} \t: {p2.ms}ms");
-Console.WriteLine($"Time (total)\t\t: {sw.Elapsed.TotalMilliseconds}ms");
-Console.WriteLine($"*************Day 4 START*************");
+Console.WriteLine($"Time (total)\t\t\t: {sw.Elapsed.TotalMilliseconds}ms");
+Console.WriteLine($"*************Day 6 START*************");
 
 (int result, double ms) part_one(string file)
 {
@@ -58,7 +58,21 @@ Console.WriteLine($"*************Day 4 START*************");
     sw.Start();
 
     var lines = File.ReadAllLines(file);
+    var times = lines[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    var distances = lines[1].Split(":", StringSplitOptions.RemoveEmptyEntries)[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    var time = Convert.ToInt64(string.Join("", times));
+    var distance = Convert.ToInt64(string.Join("", distances));
     var total = 0; 
+
+    for(int t = 1; t<= time -1; t++)
+    {
+        var x = time - t;
+        if(x * t > distance)
+        {
+            total++;
+        }
+    }
 
     sw.Stop();
 

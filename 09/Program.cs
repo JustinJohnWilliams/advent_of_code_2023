f5d20727703ab2dbbf2ade36b7e724b3b8c7a68b
Justin Williams <justinjohnwilliams@gmail.com>
2023-12-09 15:14:06 -0600

    cleanup

--- 09/Program.cs
+++ 09/Program.cs
@@ -1,18 +1,10 @@
-using System.Data;
-
-var sw = new System.Diagnostics.Stopwatch();
-sw.Start();
-
 Console.WriteLine($"*************Day 9 START*************");
 
 var p1 = part_one("input.txt");
 var p2 = part_two("input.txt");
 
-sw.Stop();
-
 Console.WriteLine($"Part 1 Result: {p1.result} \t: {p1.ms}ms");
 Console.WriteLine($"Part 2 Result: {p2.result} \t\t: {p2.ms}ms");
-Console.WriteLine($"Time (total)\t\t\t: {sw.Elapsed.TotalMilliseconds}ms");
 Console.WriteLine($"*************Day 9  DONE*************");
 
 (long result, double ms) part_one(string file)
@@ -21,19 +13,15 @@ Console.WriteLine($"*************Day 9  DONE*************");
     sw.Start();
 
     var total = 0;
-    var measurements = get_measurements(file);
-    for(int i = 0; i < measurements.Count(); i++)
+    foreach(var measurement in get_measurements(file))
     {
-        var reducedSet = measurements[i];
-        int predicted = 0;
+        var reducedSet = measurement.Value;
+        var predicted = measurement.Value.Last();
         while(!reducedSet.IsReduced())
         {
-            reducedSet = reduce(reducedSet);
+            reducedSet = reducedSet.Reduce();
             predicted += reducedSet.Last();
         }
-        //Console.WriteLine($"{i} - {string.Join(",", differentials[i])}");
-        predicted += measurements[i].Last();
-        //Console.WriteLine($"{i} - {predicted}");
         total += predicted;
     }
 
@@ -42,32 +30,22 @@ Console.WriteLine($"*************Day 9  DONE*************");
     return (total, sw.Elapsed.TotalMilliseconds);
 }
 
-List<int> reduce(List<int> set)
-{
-    return set.Skip(1).Zip(set, (next, current) => next - current).ToList();
-}
-
 (long result, double ms) part_two(string file)
 {
     var sw = new System.Diagnostics.Stopwatch();
     sw.Start();
 
     var total = 0;
-    var measurements = get_measurements(file);
-    for(int i = 0; i < measurements.Count(); i++)
+    foreach(var measurement in get_measurements(file, true))
     {
-        var reducedSet = measurements[i];
-        var history = new List<int>();
+        var reducedSet = measurement.Value;
+        var predicted = measurement.Value.Last();
         while(!reducedSet.IsReduced())
         {
-            history.Add(reducedSet.First());
-            reducedSet = reduce(reducedSet);
+            reducedSet = reducedSet.Reduce();
+            predicted += reducedSet.Last();
         }
-
-        history.Add(0);
-        history.Reverse();
-
-        total += history.Aggregate((a, b) => b - a);
+        total += predicted;
     }
 
     sw.Stop();
@@ -75,14 +53,15 @@ List<int> reduce(List<int> set)
     return (total, sw.Elapsed.TotalMilliseconds);
 }
 
-Dictionary<int, List<int>> get_measurements(string file)
+Dictionary<int, List<int>> get_measurements(string file, bool unoreverse = false)
 {
     var results = new Dictionary<int, List<int>>();
 
     var lines = File.ReadAllLines(file);
     for(int i = 0; i < lines.Length; i++)
     {
-        results.Add(i, lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(c => Convert.ToInt32(c)).ToList());
+        var nums = lines[i].Split(' ').Select(int.Parse).ToList();
+        results.Add(i, unoreverse ? nums.UnoReverse() : nums);
     }
 
     return results;
@@ -93,5 +72,21 @@ public static class Extensions
     public static bool IsReduced(this List<int> set)
     {
         return set.All(c => c == 0);
+    }
+
+    public static List<int> Reduce(this List<int> set)
+    {
+        return set.Skip(1).Zip(set, (next, current) => next - current).ToList();
+    }
+
+    public static List<int> UnoReverse(this List<int> set)
+    {
+        set.Reverse();
+        return set;
+    }
+
+    public static void PrettyPrint(this Dictionary<int, List<int>> dict)
+    {
+        Console.WriteLine(string.Join(", ", dict.Select(kv => $"{kv.Key}: [{string.Join(", ", kv.Value)}]")));
     }
 }

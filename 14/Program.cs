b9929f801cdb0886f88887af81c0c1b05ce0541b
Justin Williams <justinjohnwilliams@gmail.com>
2023-12-16 09:54:03 -0600

    day 14: part 2 (nope lol)

--- 14/Program.cs
+++ 14/Program.cs
@@ -1,7 +1,7 @@
 Console.WriteLine($"*************Day 14 START*************");
 
 var p1 = part_one("input.txt");
-var p2 = part_two("input.txt");
+var p2 = part_two("example.txt");
 
 Console.WriteLine($"Part 1 Result: {p1.result} \t: {p1.ms}ms");
 Console.WriteLine($"Part 2 Result: {p2.result} \t: {p2.ms}ms");
@@ -14,7 +14,7 @@ Console.WriteLine($"*************Day 14  DONE*************");
 
     var lines = File.ReadAllLines(file);
 
-    tilt_north(lines);
+    tilt(lines, Direction.N);
     var total = calculate_load(lines);
 
     sw.Stop();
@@ -29,43 +29,113 @@ Console.WriteLine($"*************Day 14  DONE*************");
     sw.Start();
 
     var lines = File.ReadAllLines(file);
-    var total = 0;
+
+    tilt(lines, Direction.N);
+    tilt(lines, Direction.W);
+    tilt(lines, Direction.S);
+    tilt(lines, Direction.E);
+    var total = calculate_load(lines);
+    foreach(var line in lines)
+    {
+        Console.WriteLine(line);
+    }
 
     sw.Stop();
     
     return (total, sw.Elapsed.TotalMilliseconds);
 }
 
-void tilt_north(string[] grid)
+void tilt(string[] grid, Direction d)
 {
-    var rows = grid.Length;
-    var cols = grid[0].Length;
+    int rows = grid.Length;
+    int cols = grid[0].Length;
 
-    for (int col = 0; col < cols; col++)
+    switch (d)
     {
-        for (int row = 1; row < rows; row++)
-        {
-            if (grid[row][col] == 'O')
+        case Direction.N:
+            for (int col = 0; col < cols; col++)
             {
-                int currentRow = row;
-                while (currentRow > 0 && grid[currentRow - 1][col] == '.')
+                for (int row = 1; row < rows; row++)
                 {
-                    currentRow--;
-                }
-
-                if (currentRow != row)
-                {
-                    char[] rowChars = grid[row].ToCharArray();
-                    char[] currentRowChars = grid[currentRow].ToCharArray();
-
-                    rowChars[col] = '.';
-                    currentRowChars[col] = 'O';
-
-                    grid[row] = new string(rowChars);
-                    grid[currentRow] = new string(currentRowChars);
+                    if (grid[row][col] == 'O')
+                    {
+                        int targetRow = row;
+                        while (targetRow > 0 && grid[targetRow - 1][col] == '.')
+                        {
+                            targetRow--;
+                        }
+                        if (targetRow != row)
+                        {
+                            move_rock(grid, row, col, targetRow - row, 0);
+                            row = targetRow;
+                        }
+                    }
                 }
             }
-        }
+            break;
+        case Direction.S:
+            for (int col = 0; col < cols; col++)
+            {
+                for (int row = rows - 2; row >= 0; row--)
+                {
+                    if (grid[row][col] == 'O')
+                    {
+                        int targetRow = row;
+                        while (targetRow < rows - 1 && grid[targetRow + 1][col] == '.')
+                        {
+                            targetRow++;
+                        }
+                        if (targetRow != row)
+                        {
+                            move_rock(grid, row, col, targetRow - row, 0);
+                            row = targetRow;
+                        }
+                    }
+                }
+            }
+            break;
+        case Direction.E:
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = cols - 2; col >= 0; col--)
+                {
+                    if (grid[row][col] == 'O')
+                    {
+                        int targetCol = col;
+                        while (targetCol < cols - 1 && grid[row][targetCol + 1] == '.')
+                        {
+                            targetCol++;
+                        }
+                        if (targetCol != col)
+                        {
+                            move_rock(grid, row, col, 0, targetCol - col);
+                            col = targetCol;
+                        }
+                    }
+                }
+            }
+            break;
+        case Direction.W:
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 1; col < cols; col++)
+                {
+                    if (grid[row][col] == 'O')
+                    {
+                        int targetCol = col;
+                        while (targetCol > 0 && grid[row][targetCol - 1] == '.')
+                        {
+                            targetCol--;
+                        }
+                        if (targetCol != col)
+                        {
+                            move_rock(grid, row, col, 0, targetCol - col);
+                            col = targetCol;
+                        }
+                    }
+                }
+            }
+            break;
     }
 }
 
@@ -82,3 +152,17 @@ int calculate_load(string[] grid)
 
     return totalLoad;
 }
+
+void move_rock(string[] grid, int row, int col, int dY, int dX)
+{
+    char[] currentRowChars = grid[row].ToCharArray();
+    char[] newRowChars = grid[row + dY].ToCharArray();
+
+    currentRowChars[col] = '.';
+    newRowChars[col + dX] = 'O';
+
+    grid[row] = new string(currentRowChars);
+    grid[row + dY] = new string(newRowChars);
+}
+
+enum Direction { N, S, E, W }
